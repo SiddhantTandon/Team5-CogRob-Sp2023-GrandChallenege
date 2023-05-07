Fixed nans in loss gradient functions Added loss functions (true loss, not gradient of loss) to monitor learning covergence Normalized images from 0-1 and inverted the values Set initial weights to be between 0-1

gradient_descent.py
@@ -10,6 +10,11 @@ from loss_functions import temporal_cohesion_sol
 from loss_functions import proportionality_prior_sol
 from loss_functions import causality_prior_sol
 from loss_functions import repeatability_prior_sol
+from loss_functions import temporal_loss
+from loss_functions import proportional_loss
+from loss_functions import causal_loss
+from loss_functions import repeatability_loss
+from loss_functions import multi_agent_loss
 from loss_functions import multi_prior_sol
 
 #### training set function call ####
@@ -32,27 +37,27 @@ def main():
 
     # Weight matrix setup
     agent_dim = 2 * 2 # each agent has 2D-SPACE: x-coordinate, y-coordinate
-    W = np.random.uniform(0,10,[agent_dim,100])
+    W = np.random.uniform(0,1,[agent_dim,100])
 
     # gradient descent loop
     for i in range(0, args.iterations):
 
         total_loss = 0
         # get temporal loss
-        temp_loss = temporal_cohesion_sol(batch, W)
-        total_loss += temp_loss
+        temp_loss_grad = temporal_cohesion_sol(batch, W)
+        total_loss += temp_loss_grad
 
         # get proportionality
-        prop_loss = proportionality_prior_sol(batch,W)
-        total_loss += prop_loss
+        prop_loss_grad = proportionality_prior_sol(batch,W)
+        total_loss += prop_loss_grad
 
         # get causality loss
-        causal_loss = causality_prior_sol(batch,W)
-        total_loss += causal_loss
+        causal_loss_grad = causality_prior_sol(batch,W)
+        total_loss += causal_loss_grad
 
         # get repeatability loss
-        repeat_loss = repeatability_prior_sol(batch,W)
-        total_loss += repeat_loss
+        repeat_loss_grad = repeatability_prior_sol(batch,W)
+        total_loss += repeat_loss_grad
 
         # get multi prior loss
         multi_loss = multi_prior_sol(batch,W)
@@ -61,8 +66,19 @@ def main():
         # update weights
         W = W - args.alpha * total_loss
 
+        t_loss = temporal_loss(batch,W)
+        p_loss = proportional_loss(batch,W)
+        c_loss = causal_loss(batch,W)
+        r_loss = repeatability_loss(batch,W)
+        m_loss = multi_agent_loss(batch,W)
+        loss_total = t_loss + p_loss + c_loss + r_loss + m_loss
+
         # are we reducing loss?
-        print(i, np.sum(total_loss))
+        print("{}: {}_t + {}_p + {}_c + {}_r + {}_m = {}".format(i, t_loss, p_loss, c_loss, r_loss, m_loss, loss_total))
+        print("temp: {}\nprop: {}\nCausal: {}\nRepeat: {}".format(np.isnan(temp_loss_grad).any(),
+                                                                  np.isnan(prop_loss_grad).any(),
+                                                                  np.isnan(causal_loss_grad).any(),
+                                                                  np.isnan(repeat_loss_grad).any()))
 
     # coordinates learnt
     y = W.dot(X)

loss_functions.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils_pset import Data_Frame
+from multi_agent_sim_data import DataFrame
 
 
 def action_equality(a1, a2):
@@ -12,6 +12,80 @@ def reward_equality(r1, r2):
     for i in range(len(r1)):
         if r1[i] != r2[i]:
             return False
+
+def state_delta_mag(s1, s2, mapping):
+    return np.linalg.norm(mapping@s2-mapping@s1)
+
+def temporal_loss(batch, mapping):
+    time_steps = len(batch)
+    total_loss = 0
+    for i in range(0, time_steps-1):
+        total_loss += state_delta_mag(batch[i+1].image,batch[i].image, mapping)**2
+    return total_loss/(time_steps-1)
+
+def proportional_loss(batch,mapping):
+    time_steps = len(batch)
+    total_loss = 0
+    pairings = 0
+    for i in range(0, time_steps - 1):
+        for j in range(i + 1, time_steps - 1):
+            if batch[i].action == batch[j].action:
+                pairings += 1
+                delta1 = state_delta_mag(batch[i+1].image, batch[i].image, mapping)
+                delta2 = state_delta_mag(batch[j+1].image, batch[j].image, mapping)
+                total_loss += (delta2- delta1)**2
+    if pairings == 0:
+        return 0
+    return total_loss/pairings
+
+def causal_loss(batch,mapping):
+    time_steps = len(batch)
+    total_loss = 0
+    pairings = 0
+    for i in range(0, time_steps - 1):
+
+        for j in range(i + 1, time_steps - 1):
+            if batch[i].action == batch[j].action and batch[i].reward == batch[j].reward:
+                pairings += 1
+                delta = state_delta_mag(batch[i].image, batch[j].image, mapping)
+                total_loss += np.exp(-delta)
+
+    if pairings == 0:
+        return 0
+    return total_loss/pairings
+
+def repeatability_loss(batch,mapping):
+    time_steps = len(batch)
+    total_loss = 0
+    pairings = 0
+    for i in range(0, time_steps - 1):
+        for j in range(i + 1, time_steps - 1):
+            if batch[i].action == batch[j].action:
+                pairings += 1
+                causal_part = np.exp(-state_delta_mag(batch[i].image, batch[j].image, mapping))
+                delta1 = mapping@batch[i+1].image - mapping@batch[i].image
+                delta2 = mapping@batch[j+1].image - mapping@batch[j].image
+                total_loss += causal_part*(np.linalg.norm(delta2 - delta1)**2)
+    if pairings == 0:
+        return 0
+    return total_loss/pairings
+
+def multi_agent_loss(batch,mapping):
+    time_steps = len(batch)
+    total_loss = 0
+    counter = 0
+    for i in range(0, time_steps - 1):
+        if batch[i].action[0] != batch[i].action[1]:
+            counter += 1
+            delta1 = (mapping @ batch[i + 1].image)[0:2] - (mapping @ batch[i].image)[0:2]
+            delta2 = (mapping @ batch[i + 1].image)[2:4] - (mapping @ batch[i].image)[2:4]
+            total_loss += np.exp(-np.linalg.norm(delta2 - delta1))
+
+    if counter == 0:
+        return 0
+    return total_loss / counter
+
+
 
 def temporal_cohesion_sol(batch, mapping):
     """
@@ -58,7 +132,7 @@ def proportionality_prior_sol(batch, mapping):
     pairings = 0
     for i in range(0, time_steps - 1):
         for j in range(i+1,time_steps - 1):
-            if action_equality(batch[i].action, batch[j].action):
+            if batch[i].action == batch[j].action:
                 pairings += 1
                 loss_grad = proportional_loss_gradient(batch[i].image, batch[i + 1].image, batch[j].image, batch[j+1].image, mapping)
                 total_loss_grad += loss_grad.reshape(mapping.shape)
@@ -102,12 +176,15 @@ def causality_prior_sol(batch, mapping):
     for i in range(0, time_steps - 1):
 
         for j in range(i + 1, time_steps - 1):
-            pairings += 1
-            if action_equality(batch[i].action, batch[j].action) and reward_equality(batch[i].reward, batch[j].reward):
+            if batch[i].action == batch[j].action and batch[i].reward == batch[j].reward:
                 loss_grad = causal_loss_gradient(batch[i].image, batch[j].image,
                                                 mapping)
+                pairings += 1
                 total_loss_grad += loss_grad.reshape(mapping.shape)
 
+    print("pairings: {}".format(pairings))
+    if pairings == 0:
+        return np.zeros(mapping.shape)
     return total_loss_grad / pairings
 
 def causal_loss_gradient(s1,s2,mapping):
@@ -118,9 +195,13 @@ def causal_loss_gradient(s1,s2,mapping):
     delta_norm = np.linalg.norm(delta)
     for i in range(0,dims[0]):
         denom = 2*delta[i]*delta_norm
+
         for j in range(0,dims[1]):
             numer = delta[i]*np.exp(-delta_norm) * (s1[j] - s2[j])*delta[i]*2
-            output[i,j] = -numer/denom
+            if denom == 0:
+                output[i,j] = 0
+            else:
+                output[i,j] = -numer/denom
     return output
 
 def repeatability_prior_sol(batch, mapping):
@@ -131,7 +212,7 @@ def repeatability_prior_sol(batch, mapping):
         for j in range(i + 1, time_steps - 1):
             if batch[i].action == batch[j].action:
                 pairings += 1
-                loss_grad = proportional_loss_gradient(batch[i].image, batch[i + 1].image, batch[j].image,
+                loss_grad = repeatability_loss_gradient(batch[i].image, batch[i + 1].image, batch[j].image,
                                                        batch[j + 1].image, mapping)
                 total_loss_grad += loss_grad.reshape(mapping.shape)
 
@@ -166,10 +247,12 @@ def multi_prior_sol(batch, mapping):
     pairings = 0
     counter = 0
     for i in range(0,time_steps-1):
-        if batch[i].action[0] == batch[i].action[1]:
+        if batch[i].action[0] != batch[i].action[1]:
             counter += 1
             total_loss_grad += multi_loss_gradient(batch[i].image, batch[i+1].image, mapping)
 
+    if counter == 0:
+        return np.zeros(mapping.shape)
     return total_loss_grad/counter
 
 
@@ -197,12 +280,12 @@ if __name__ == "__main__":
     image2 = np.array([1, 0, 1, 1]).T  # (1,0)
     image3 = np.array([1, 1, 0, 1]).T  # (0,0)
     image4 = np.array([1, 1, 1, 0.1]).T  # (1,0)
-    frame1 = Data_Frame(np.array([1, 2]), 4, 1, image1)
-    frame2 = Data_Frame(np.array([2, 3]), 4, 2, image2)
-    frame3 = Data_Frame(np.array([2, 1]), 4, 1, image3)
-    frame4 = Data_Frame(np.array([2, 4]), 4, 2, image4)
-    multiframe1 = Data_Frame(np.array([1, 2]), (4,4), 1, image1)
-    multiframe2 = Data_Frame(np.array([1, 2]), (4,4), 1, image2)
+    frame1 = DataFrame(np.array([1, 2]), 4, 1, image1)
+    frame2 = DataFrame(np.array([2, 3]), 4, 2, image2)
+    frame3 = DataFrame(np.array([2, 1]), 4, 1, image3)
+    frame4 = DataFrame(np.array([2, 4]), 4, 2, image4)
+    multiframe1 = DataFrame(np.array([1, 2]), (4,4), 1, image1)
+    multiframe2 = DataFrame(np.array([1, 2]), (4,4), 1, image2)
     mapping = np.arange(16).reshape((4, 4))
     mapping[0,2] = 0
     mapping[0,1] = 0